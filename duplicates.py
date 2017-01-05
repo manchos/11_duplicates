3330bb42ba189e8cacce0ae918230739bb874101
manchos <vladm.ru@gmail.com>
2017-01-06 01:03:53 +0300

    search in 2 directories

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,21 @@
+import os
+from collections import Counter
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def are_files_duplicates(dir_path1, dir_path_2):
+    search_dublicates(search_dublicates(dir_path1)+search_dublicates(dir_path_2))
+
+
+def search_dublicates(path):
+    path_f = []
+    for dir_path, dirs, files in os.walk(path):
+        for file_name in files:
+            file_path = os.path.join(dir_path, file_name)
+            file_size = os.path.getsize(file_path)
+            path_f.append((file_name, file_size))
+    return [pair[0] for pair in Counter(path_f).most_common() if pair[1] > 1]
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Enter the real path to json file with list of bars:')
+    print(search_dublicates(filepath))
