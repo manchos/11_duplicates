91101ba0cb0f2c869ef11d5ecc240a3fca25ec19
manchos <vladm.ru@gmail.com>
2017-01-16 14:26:46 +0300

    rename variable

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 # Anti-Duplicator
 
 Script search and display duplicates in folder.
+
 Script get folder in which you want to remove duplicates and display all duplicate files with file path.
 
-# Project Goals
+## Using
+Run the script like python duplicates.py
+
+enter the directory path
+
+get duplicates like this:
+
+Found n duplicates:
+
+File: file_1 found in:
+
+/file_path/file_1
+
+.
+
+.
+
+/file_path/file_n
+
+## Requirement
+
+Python >=3.5
+
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
--- duplicates.py
+++ duplicates.py
@@ -2,28 +2,31 @@ import os
 
 
 def search_dublicates(path):
+    # data structure :
+    # {'file1_name/file1_size': [file1_path_1, file1_path_2], 'file2_name/file2_size': [file2_path_1]}
     file_dict_by_name_size_path = {}
     for dir_path, dirs, files in os.walk(path):
         for file_name in files:
             file_path = os.path.join(dir_path, file_name)
             file_size = os.path.getsize(file_path)
-            file_name_size = str(file_name) + '_' + str(file_size)
-            if file_name_size not in file_dict_by_name_size_path:
-                file_dict_by_name_size_path[file_name_size] = [file_path]
+            file_name_size_key = str(file_name) + '/' + str(file_size)
+            if file_name_size_key not in file_dict_by_name_size_path:
+                file_dict_by_name_size_path[file_name_size_key] = [file_path]
             else:
-                file_dict_by_name_size_path[file_name_size].append(file_path)
-    return [i for i in file_dict_by_name_size_path.items() if len(i[1]) > 1]
+                file_dict_by_name_size_path[file_name_size_key].append(file_path)
+    return [file_info for file_info in file_dict_by_name_size_path.items() if len(file_info[1]) > 1]
 
 
 def print_duplicates_to_terminal(duplicates):
-    for i in duplicates:
-        print('\n File: %s found in:' % (i[0].split('_')[0]))
-        for dub in i[1]:
-            print(dub)
+    for file_info in duplicates:
+        file_name = file_info[0].split('/')[0]
+        print('\n File: %s found in:' % file_name)
+        for file_path in file_info[1]:
+            print(file_path)
 
 
 if __name__ == '__main__':
-    dir_path = input('Enter the real path to directory:')
+    dir_path = input('Enter the real path to directory name: ')
     if not os.path.exists(dir_path):
         print('Folder not fount')
         exit()
