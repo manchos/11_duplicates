4f515aa106dc9d6c97a6545a08d1e63cd3cd6551
manchos <vladm.ru@gmail.com>
2017-01-18 00:18:59 +0300

    work with namedtuple and defaultdict

--- duplicates.py
+++ duplicates.py
@@ -14,24 +14,19 @@ def search_dublicates(path):
         for file_name in files:
             file_path = os.path.join(dir_path, file_name)
             file_size = os.path.getsize(file_path)
-            file_name_size_key = str(file_name) + '/' + str(file_size)
-            if file_name_size_key not in file_dict_by_name_size_path:
-                file_dict_by_name_size_path[file_name_size_key] = [file_path]
-                d[(file.name, file.size)].append(path)
-            else:
-                file_dict_by_name_size_path[file_name_size_key].append(file_path)
-    return [file_info for file_info in file_dict_by_name_size_path.items() if len(file_info[1]) > 1]
+            file_dict_by_name_size_path[FileInfo(file_name, file_size)].append(file_path)
+    return dict(file_info for file_info in file_dict_by_name_size_path.items() if len(file_info[1]) > 1)
 
 
 def print_duplicates_to_terminal(duplicates):
-    for file_info in duplicates:
-        file_name = file_info[0].split('/')[0]
-        print('\n File: %s found in:' % file_name)
-        for file_path in file_info[1]:
-            print(file_path)
+    for file_info, file_paths in duplicates.items():
+        print('\n File: %s found in:' % file_info.name)
+        for path in file_paths:
+            print(path)
 
 
 if __name__ == '__main__':
+
     dir_path = input('Enter the real path to directory name: ')
     if not os.path.exists(dir_path):
         print('Folder not fount')
